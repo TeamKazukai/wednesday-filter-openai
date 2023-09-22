ae16e3305f45615ab790050c5d49542f40823380
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-09-22 11:17:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devil-Botz/Elsa.git /Elsa
+  git clone https://github.com/TeamKazukai/wednesday-filter-openai.git /wednesday-filter-openai
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /wednesday-filter-openai
 fi
-cd /Elsa
+cd /wednesday-filter-openai
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 wednesday....🧞‍♂️"
 python3 bot.py
